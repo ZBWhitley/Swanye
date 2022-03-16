bd407983bfd2cf9c833b688f4917408b6532b29a
Zachary Whitley <zbwhitley@gmail.com>
2022-03-16 13:44:02 -0500

    Add project files.

---
+++ Swanye/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Swanye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGen(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("So Yeezy was all like");
+                Console.WriteLine($"Yeezy: {quote.Kanye()}");
+                Console.WriteLine("and then RonSon was all like");
+                Console.WriteLine($"RonSon: {quote.RonSwanson()}");
+
+            }
+        }
+    }
+}
---
+++ Swanye/QuoteGen.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swanye
+{
+    internal class QuoteGen
+    {
+        private HttpClient _client;
+
+        public QuoteGen(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            //API Url
+            var yeezy = "https://api.kanye.rest";
+
+            //Stores the JSON repsonse in a variable
+            var yeezyResponse = _client.GetStringAsync(yeezy).Result;
+
+            //Parses through the response we received to get the value associated with
+            //the NAME "quote"
+            var yeezyQuote = JObject.Parse(yeezyResponse).GetValue("quote").ToString();
+
+            return yeezyQuote;
+        }
+
+        public string RonSwanson()
+        {
+            //Ron Swanson URL
+            var ronSon = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronSonResponse = _client.GetStringAsync(ronSon).Result;
+            var ronSonQuote = JArray.Parse(ronSonResponse);
+
+            return ronSonQuote[0].ToString();
+        }
+    }
+}
